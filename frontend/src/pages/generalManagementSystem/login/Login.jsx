b08b96fb621f31228c65f2518fd2523b80e21c49
Malitha Prabhashana <malithaprabhashana@gmail.com>
2024-04-19 09:19:08 +0530

    File Structure

--- frontend/src/pages/generalManagementSystem/login/Login.jsx
+++ frontend/src/pages/generalManagementSystem/login/Login.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './Login.css'
 import TextField from '@mui/material/TextField';
 import Grid from '@mui/material/Grid';
 import axios from 'axios';
 import Alert from '@mui/material/Alert';
-import { Link,useNavigate } from "react-router-dom";
+import MenuItem from '@mui/material/MenuItem';
+import { Link, useNavigate, useParams } from "react-router-dom";
 import { useState } from "react";
 import { forwardRef, useImperativeHandle } from 'react';
 import { motion } from "framer-motion";
@@ -13,16 +14,28 @@ import bg1 from '../../../assets/bg1.jpeg';
 
 
 
-
-
-
-function Login() {
+const Login = (props) => {
 
     const navigate = useNavigate();
     const [loginEmail, setLoginEmail] = useState('');
     const [loginPassword, setLoginPassword] = useState('');
+    const [managerPosition, setManagerPosition] = useState('');
     const [showAlert, setShowAlert] = useState(null);
 
+
+    const positions = [
+        { value: 'general_manager', label: 'General Manager' },
+        { value: 'inventory_manager', label: 'Inventory Manager' },
+        { value: 'human_resource_manager', label: 'Human Resource Manager' },
+        { value: 'customer_order_manager', label: 'Customer Order Manager' },
+        { value: 'financial_manager', label: 'Financial Manager' },
+        { value: 'logistic_manager', label: 'Logistic Manager' },
+        { value: 'manufacturing_manager', label: 'Manufacturing Manager' },
+        { value: 'quality_assurance_manager', label: 'Quality Assurance Manager' },
+        { value: 'sales_manager', label: 'Sales Manager' },
+        { value: 'training_development_manager', label: 'Training and Development Manager' },
+    ];
+    
 
     const submitLogForm = () => {
         if (loginEmail !== "" && loginPassword !== "") {
@@ -35,14 +48,14 @@ function Login() {
 
             axios.post('http://localhost:3001/user/login', loginFormData).then(response => {
                 const responseStatus = response.status;
-                  console.log(response.data)
+                console.log(response.data)
                 if (responseStatus === 200 | responseStatus === 201) {
                     localStorage.setItem('token', response.data['token']);
                     localStorage.setItem('role', response.data['role']);
                     localStorage.setItem('store', response.data['store']);
-                    if (response.data.status===0){
+                    if (response.data.status === 0) {
                         navigate('/');
-                    }else{
+                    } else {
                         navigate('/dashboard');
                     }
 
@@ -60,8 +73,6 @@ function Login() {
 
 
 
-
-
     return (
 
         <section id="sectionLogin">
@@ -69,7 +80,8 @@ function Login() {
             <div className="overlay"></div>
             <motion.div animate={{ y: 0, opacity: 1 }} initial={{ opacity: 0, y: 30 }} transition={{ delay: 0.3, duration: 0.5 }} className="logIn ">
 
-                <h2>Log In</h2>
+                <h2>{props.role && props.role === "manager" ? "Manager Login" : props.role === "customer" ? "Customer Login" : ""}</h2>
+
                 {showAlert && (
                     <Alert severity="error">Error -<strong>{showAlert}</strong></Alert>
                 )}
@@ -90,14 +102,35 @@ function Login() {
                             id="password" label="Password" variant="outlined" type="password" fullWidth />
                     </Grid>
                 </Grid>
+
+                {props.role && props.role === "manager" && (
+                    <Grid container spacing={2}>
+                        <Grid item xs={12}>
+                            <TextField
+                                select
+                                fullWidth
+                                label="Position"
+                                value={managerPosition}
+                                onChange={e => setManagerPosition(e.target.value)}
+                                variant="outlined"
+                            >
+                                {positions.map(position => (
+                                    <MenuItem key={position.value} value={position.value}>
+                                        {position.label}
+                                    </MenuItem>
+                                ))}
+
+                            </TextField>
+                        </Grid>
+                    </Grid>
+                )}
+
                 <div className="inputBox">
                     <input type="submit" value="Log In" id="btn" onClick={submitLogForm} />
                 </div>
                 <div className="group">
-                    <Link to="/" className='nav-avatar-list'> Forgot password</Link>
+                    <Link to="/" className='nav-avatar-list'>Forgot password</Link>
                     <Link to="/register" className='nav-avatar-list'> Sign Up</Link>
-                    {/* <a href="#">Forgot password</a>
-                    <a href="#">Sign Up</a> */}
                 </div>
 
 
@@ -109,4 +142,4 @@ function Login() {
 }
 
 
-export default forwardRef(Login)
+export default Login;
