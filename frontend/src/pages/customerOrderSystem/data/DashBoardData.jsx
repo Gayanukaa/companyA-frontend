1b7b67874cf65736d9d4e48e26cfe3abcf25e74c
ManuliGavinya <162779174+ManuliGavinya@users.noreply.github.com>
2024-04-27 12:07:56 +0530

    updated dashboard change

--- frontend/src/pages/customerOrderSystem/data/DashBoardData.jsx
+++ frontend/src/pages/customerOrderSystem/data/DashBoardData.jsx
@@ -13,6 +13,6 @@ export const personImages=[per1,per2,per3,per4,per5,per6,per7,per8]
 
 export const dashboardAdminData = [
     { name: "Dashboard", icon: <i className='bx bxs-dashboard' ></i>, active: true, to: 'dashboard' },
-    { name: "Place Orde r", icon: <i className='bx bxs-user-plus'></i>, active: false, to: 'view-stocks' },
+    { name: "Place Order", icon: <i className='bx bxs-user-plus'></i>, active: false, to: 'view-stocks' },
     { name: "View Stocks", icon: <i className='bx bxs-user-plus'></i>, active: false, to: 'view-stocks' }
 ]
