aa2012d866c25616571fbdf68725e83fe24d9a13
dnmanamperi <dnmanamperi@gmail.com>
2024-04-29 18:35:12 +0530

    update course forms
    add, edit, view

--- frontend/src/pages/trainingAndDevelopmentSystem/manager/SideBarPages.jsx
+++ frontend/src/pages/trainingAndDevelopmentSystem/manager/SideBarPages.jsx
@@ -281,3 +281,259 @@ export function AddMainSupervisor(props) {
         </div>
     );
 }
+
+export function Course(props) {
+
+    const [courses, setCourses] = useState([]);
+
+    useEffect(() => {
+        loadCourses();
+    }, []);
+
+    const loadCourses = async () => {
+        try {
+            const result = reqSend.defaultReq('GET', 'api/tms/courses', null, (response) => {
+                setCourses(response.data);
+            }, (error) => {
+                console.error('Error loading courses:', error);
+            });
+        } catch (error) {
+            console.error('Error loading courses:', error);
+        }
+    };
+
+    const deleteCourses = async (id) => {
+        try {
+            reqSend.defaultReq('DELETE', `api/tms/course/${id}`, null, () => {
+                loadCourses();
+            }, (error) => {
+                console.error('Error deleting course:', error);
+            });
+        } catch (error) {
+            console.error('Error deleting course:', error);
+        }
+    };
+
+    const tableData = {
+        heading: ["", "ID", "Course Name", "Instructor", "Cost (LKR)", "Duration (Months)","",""],
+        body: courses.map((course, index) => (
+            <tr key={index}>
+                <th scope="row">{index + 1}</th>
+                <td>{course.courseId}</td>
+                <td>{course.courseName}</td>
+                <td>{course.instructor}</td>
+                <td>{course.cost}</td>
+                <td>{course.duration}</td>
+                <td>
+                    <button className="btn btn-danger mx-2" onClick={() => deleteCourses(course.id)}>Delete</button>
+                </td>
+                <td>
+                    <Link className="btn btn-outline-primary mx-2" to={`edit/${course.id}`}>Edit</Link>
+                </td>
+            </tr>
+        )),
+    };
+
+    return (
+        <>
+            <main>
+                <div className="head-title">
+                    <div className="left">
+                        <h1>Courses</h1>
+                        <Link className="btn btn-primary mx-2" to={"add"}>Add</Link>
+                    </div>
+                    <TableComp data={tableData} />
+                </div>
+            </main>
+        </>
+    )
+}
+
+export function AddCourse(props){
+    let navigate = useNavigate();
+
+    const [user, setUser] = useState({
+        "courseId": "",
+        "courseName": "",
+        "link": "",
+        "instructor": "",
+        "details": "",
+        "cost":"",
+        "duration":"",
+    });
+
+    const { courseId, courseName, link, instructor, details,cost,duration } = user;
+
+    const onInputChange = (e) => {
+        setUser({ ...user, [e.target.name]: e.target.value });
+    };
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const userData = { ...user, courseId: parseInt(courseId),duration: parseInt(duration),cost: parseFloat(cost) };
+            reqSend.defaultReq('POST', 'api/tms/course', userData, () => {
+                navigate("/trainingdevelopment-management/manager/courses");
+            }, (error) => {
+                console.error('Error adding main supervisor:', error);
+            });
+        } catch (error) {
+            console.error('Error adding main supervisor:', error);
+        }
+    };
+
+    return (
+        <div className='container'>
+        <div className="row">
+            <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow" style={{  color: '#007bff' }}>
+                <h2 className="text-center m-4">Add Course Details</h2>
+                <form onSubmit={(e)=>onSubmit(e)} action="">
+                <div className="mb-3">
+                    <label htmlFor="Name" className='form-label'>Course ID</label>
+                    <input type={"number"} className='form-control'name='courseId' required placeholder='Enter course ID'value={courseId} onChange={(e)=>onInputChange(e)}/>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="Username" className='form-label'>Course Name</label>
+                    <input type={"text"} className='form-control'name='courseName' required placeholder='Enter course name' value={courseName} onChange={(e)=>onInputChange(e)}/>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="Username" className='form-label'>Instructor</label>
+                    <input type={"text"} className='form-control'name='instructor' required placeholder='Enter Instructor' value={instructor} onChange={(e)=>onInputChange(e)}/>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="Username" className='form-label'>Details</label>
+                    <input type={"text"} className='form-control'name='details' required placeholder='Enter details' value={details} onChange={(e)=>onInputChange(e)}/>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="Username" className='form-label'>Link to Learn</label>
+                    <input type={"url"} className='form-control'name='link' required placeholder='Enter link' value={link} onChange={(e)=>onInputChange(e)}/>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="Username" className='form-label'>Cost (LKR)</label>
+                    <input type={"text"} className='form-control'name='cost' required placeholder='Enter cost' value={cost} onChange={(e)=>onInputChange(e)}/>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="Username" className='form-label'>Duration (Months)</label>
+                    <input type={"number"} className='form-control'name='duration' required placeholder='Enter duration' value={duration} onChange={(e)=>onInputChange(e)}/>
+                </div>
+                
+                <div>
+                    <button type='submit' className='btn btn-outline-primary'>Submit</button>
+                    <Link type='submit' className='btn btn-outline-danger mx-2'to='/trainingdevelopment-management/manager/courses' >Cancel</Link>
+                </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    );
+}
+
+export function EditCourse(props){
+    let navigate=useNavigate();
+    const {id}=useParams();
+
+    const [user, setUser] = useState({
+        "courseId": "",
+        "courseName": "",
+        "link": "",
+        "instructor": "",
+        "details": "",
+        "cost":"",
+        "duration":"",
+            
+    });
+
+    const { courseId, courseName, link, instructor, details,cost,duration } = user;
+
+
+        const onInputChange=(e)=>{
+            setUser({...user,[e.target.name]:e.target.value})
+        }
+
+        useEffect(()=>{
+            loadUser();
+        },[])
+        
+        // const onSubmit=async(e)=>{
+        //     e.preventDefault()
+        //     const userData = { ...user, courseId: parseInt(courseId) };
+        //     await axios.put(`http://localhost:8090/course/${id}`,userData)
+        //     navigate("/")
+        // }
+
+        // const loadUser =async()=>{
+        //     const result=await axios.get(`http://localhost:8090/courses/${id}`)
+        //     setUser(result.data)
+        // }
+
+        const loadUser = async () => {
+            try {
+                const result =  reqSend.defaultReq('GET', `api/tms/course/${id}`, null, (response) => {
+                    setUser(response.data);
+                }, (error) => {
+                    console.error('Error loading courses:', error);
+                });
+            } catch (error) {
+                console.error('Error loading courses:', error);
+            }
+        };
+
+        const onSubmit = async (e) => {
+            e.preventDefault();
+            try {
+                const userData = { ...user, courseId: parseInt(courseId),duration: parseInt(duration),cost: parseFloat(cost) };
+                reqSend.defaultReq('PUT', `api/tms/course/${id}`, userData, () => {
+                    navigate("/trainingdevelopment-management/manager/courses");
+                }, (error) => {
+                    console.error('Error adding main supervisor:', error);
+                });
+            } catch (error) {
+                console.error('Error adding main supervisor:', error);
+            }
+        };
+
+  return (
+    <div className='container'>
+        <div className="row">
+            <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow" style={{  color: '#007bff' }}>
+                <h2 className="text-center m-4">Edit Course Details</h2>
+                <form onSubmit={(e)=>onSubmit(e)} action="">
+                <div className="mb-3">
+                    <label htmlFor="Name" className='form-label'>Course ID</label>
+                    <input type={"number"} className='form-control'name='courseId' required placeholder='Enter course ID'value={courseId} onChange={(e)=>onInputChange(e)}/>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="Username" className='form-label'>Course Name</label>
+                    <input type={"text"} className='form-control'name='courseName'required  placeholder='Enter course name' value={courseName} onChange={(e)=>onInputChange(e)}/>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="Username" className='form-label'>Instructor</label>
+                    <input type={"text"} className='form-control'name='instructor'required  placeholder='Enter Instructor' value={instructor} onChange={(e)=>onInputChange(e)}/>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="Username" className='form-label'>Details</label>
+                    <input type={"text"} className='form-control'name='details' required placeholder='Enter details' value={details} onChange={(e)=>onInputChange(e)}/>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="Username" className='form-label'>Link to Learn</label>
+                    <input type={"url"} className='form-control'name='link' required placeholder='Enter link' value={link} onChange={(e)=>onInputChange(e)}/>
+                </div>
+                {/* <div className="mb-3">
+                    <label htmlFor="Username" className='form-label'>Cost</label>
+                    <input type={"text"} className='form-control'name='cost' required placeholder='Enter link' value={cost} onChange={(e)=>onInputChange(e)}/>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="Username" className='form-label'>Duration</label>
+                    <input type={"text"} className='form-control'name='duration' required placeholder='Enter link' value={duration} onChange={(e)=>onInputChange(e)}/>
+                </div> */}
+
+                <div>
+                    <button type='submit' className='btn btn-outline-primary'>Submit</button>
+                    <Link type='submit' className='btn btn-outline-danger mx-2'to='/trainingdevelopment-management/manager/courses' >Cancel</Link>
+                </div>
+                </form>
+            </div>
+        </div>
+    </div>
+  )
+}
