f9c4836a1eba72caac954745be5a3c3242578814
dnmanamperi <dnmanamperi@gmail.com>
2024-04-29 18:33:57 +0530

    update with add main and unit supervisors form

--- frontend/src/pages/trainingAndDevelopmentSystem/manager/SideBarPages.jsx
+++ frontend/src/pages/trainingAndDevelopmentSystem/manager/SideBarPages.jsx
@@ -163,3 +163,121 @@ export function UnitSupervisors(props) {
         </main>
     );
 }
+
+export function AddUnitSupervisor(props) {
+
+    const navigate = useNavigate();
+
+    const [user, setUser] = useState({
+        uSupervisorId: "",
+        uSupervisorEmail: "",
+        uPassword: ""
+    });
+
+    const { uSupervisorId, uSupervisorEmail, uPassword } = user;
+
+    const onInputChange = (e) => {
+        setUser({ ...user, [e.target.name]: e.target.value });
+    };
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const userData = { ...user, uSupervisorId: parseInt(uSupervisorId) };
+            reqSend.defaultReq('POST', 'api/tms/unit-supervisor', userData, () => {
+                navigate("/trainingdevelopment-management/manager/unit-supervisors");
+            }, (error) => {
+                console.error('Error adding unit supervisor:', error);
+            });
+        } catch (error) {
+            console.error('Error adding unit supervisor:', error);
+        }
+    };
+
+    return (
+        <div className='container'>
+            <div className="row">
+                <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow" style={{  color: '#007bff' }}>
+                    <h2 className="text-center m-4">Add Unit Supervisor</h2>
+                    <form onSubmit={(e) => onSubmit(e)} action="">
+                        <div className="mb-3">
+                            <label htmlFor="Name" className='form-label'>Supervisor Email</label>
+                            <input type="text" className='form-control' name='uSupervisorEmail' required placeholder='Enter Supervisor Email' value={uSupervisorEmail} onChange={(e) => onInputChange(e)} />
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="Username" className='form-label'>Supervisor ID</label>
+                            <input type="number" className='form-control' name='uSupervisorId' required placeholder='Enter Supervisor Id' value={uSupervisorId} onChange={(e) => onInputChange(e)} />
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="Username" className='form-label'>Password</label>
+                            <input type="password" className='form-control' name='uPassword' required placeholder='Enter Password' value={uPassword} onChange={(e) => onInputChange(e)} />
+                        </div>
+                        <div>
+                            <button type='submit' className='btn btn-outline-primary'>Submit</button>
+                            <Link type='submit' className='btn btn-outline-danger mx-2' to='/trainingdevelopment-management/manager/unit-supervisors'>Cancel</Link>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export function AddMainSupervisor(props) {
+
+    let navigate = useNavigate();
+
+    const [user, setUser] = useState({
+        mSupervisorId: "",
+        mSupervisorEmail: "",
+        mPassword: ""
+    });
+
+    const { mSupervisorId, mSupervisorEmail, mPassword } = user;
+
+    const onInputChange = (e) => {
+        setUser({ ...user, [e.target.name]: e.target.value });
+    };
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const userData = { ...user, mSupervisorId: parseInt(mSupervisorId) };
+            reqSend.defaultReq('POST', 'api/tms/main-supervisor', userData, () => {
+                navigate("/trainingdevelopment-management/manager/main-supervisors");
+            }, (error) => {
+                console.error('Error adding main supervisor:', error);
+            });
+        } catch (error) {
+            console.error('Error adding main supervisor:', error);
+        }
+    };
+
+    return (
+        <div className='container'>
+            <div className="row">
+                <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow" style={{  color: '#007bff' }}>
+                    <h2 className="text-center m-4">Add Main Supervisor</h2>
+                    <form onSubmit={(e) => onSubmit(e)} action="">
+                        <div className="mb-3">
+                            <label htmlFor="Name" className='form-label'>Supervisor Name</label>
+                            <input type="text" className='form-control' name='mSupervisorEmail' required placeholder='Enter Supervisor Email' value={mSupervisorEmail} onChange={(e) => onInputChange(e)} />
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="Username" className='form-label'>Supervisor ID</label>
+                            <input type="number" className='form-control' name='mSupervisorId' required placeholder='Enter Supervisor Id' value={mSupervisorId} onChange={(e) => onInputChange(e)} />
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="Username" className='form-label'>Password</label>
+                            <input type="password" className='form-control' name='mPassword' required placeholder='Enter Password' value={mPassword} onChange={(e) => onInputChange(e)} />
+                        </div>
+                        <div>
+                            <button type='submit' className='btn btn-outline-primary'>Submit</button>
+                            <Link type='submit' className='btn btn-outline-danger mx-2' to='/trainingdevelopment-management/manager/main-supervisors'>Cancel</Link>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+}
