505bd828f27379ea8ab9380639a75bd43df4df4d
Malitha Prabhashana <malithaprabhashana@gmail.com>
2024-05-03 10:00:13 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,13 +4,6 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom';
 
-// const cors = require("cors");
-// App.arguments(
-//   cors({
-//     origin:"http://localhost:5173",
-//   })
-// );
-
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
