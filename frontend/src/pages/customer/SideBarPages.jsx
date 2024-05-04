8b292872933b9e8285215ad52dfdfb3d52fb37a3
Malitha Prabhashana <malithaprabhashana@gmail.com>
2024-05-04 12:12:19 +0530

    Update SideBarPages.jsx

--- frontend/src/pages/customer/SideBarPages.jsx
+++ frontend/src/pages/customer/SideBarPages.jsx
@@ -50,59 +50,59 @@ export function DashboardView(props) {
     // User detail connect with backend
     const [userData, setUserData] = useState(null);
     const [error, setError] = useState(null);
-  
+
     useEffect(() => {
-      const fetchUserData = async () => {
-        try {
-          const response = await fetch("http://localhost:8090/api/v1/userDetails?user_ID=6627402520d6be942d372b68");
-          if (!response.ok) {
-            throw new Error("Failed to fetch user details");
-          }
-          const data = await response.json();
-          setUserData(data);
-        } catch (error) {
-          setError(error.message);
-        }
-      };
-  
-      fetchUserData();
+        const fetchUserData = async () => {
+            try {
+                const response = await axios.get("http://localhost:8090/api/v1/userDetails", {
+                    params: {
+                        user_ID: "6627402520d6be942d372b68"
+                    }
+                });
+                setUserData(response.data);
+            } catch (error) {
+                setError(error.message);
+            }
+        };
+
+        fetchUserData();
     }, []);
-  
+
     return (
-      <main style={{ 
-        display: 'flex', 
-        justifyContent: 'center', 
-        alignItems: 'center', 
-        height: '100vh', 
-        backgroundPosition: 'center',
-      }}>
-        <div style={{ 
-      position: 'absolute', 
-      top: '50%', 
-      left: '50%', 
-      transform: 'translate(-50%, -50%)', 
-      backgroundColor: 'rgba(255, 255, 255, 0.8)',
-      padding: '20px', 
-      borderRadius: '10px', 
-      boxShadow: '0 0 10px rgba(0, 0, 0, 0.3)', 
-      textAlign: 'center', 
-    }}>
-        {userData ? (
-            <div>
-              <h2>About me</h2>
-              <p>First Name: {userData.firstName}</p>
-              <p>Last Name: {userData.lastName}</p>
-              <p>Contact Number: {userData.mobileNumber}</p>
-              <p>Email: {userData.email}</p>
+        <main style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100vh',
+            backgroundPosition: 'center',
+        }}>
+            <div style={{
+                position: 'absolute',
+                top: '50%',
+                left: '50%',
+                transform: 'translate(-50%, -50%)',
+                backgroundColor: 'rgba(255, 255, 255, 0.8)',
+                padding: '20px',
+                borderRadius: '10px',
+                boxShadow: '0 0 10px rgba(0, 0, 0, 0.3)',
+                textAlign: 'center',
+            }}>
+                {userData ? (
+                    <div>
+                        <h2>About me</h2>
+                        <p>First Name: {userData.firstName}</p>
+                        <p>Last Name: {userData.lastName}</p>
+                        <p>Contact Number: {userData.mobileNumber}</p>
+                        <p>Email: {userData.email}</p>
+                    </div>
+                ) : (
+                    <p>Loading user data...</p>
+                )}
+                {error && <p>Error: {error}</p>}
             </div>
-          ) : (
-            <p>Loading user data...</p>
-          )}
-          {error && <p>Error: {error}</p>}
-        </div>
-      </main>
+        </main>
     );
-  }
+}
 
 export function PlaceOrder(props) {
     // send the shipping data to backend
@@ -236,125 +236,124 @@ export function OrderHistory() {
     const [orderId, setorderId] = useState();
     const [orderStatuses, setOrderStatuses] = useState({});
     const navigate = useNavigate();
-  
+
     useEffect(() => {
-      // Fetch data from backend when component mounts
-      axios
-        .get(
-          `http://localhost:8090/api/v1/salesRecord?user_ID=${
-            localStorage.getItem("userId")
-              ? localStorage.getItem("userId")
-              : "66189fd589a3a818a791a9c2"
-          }`
-        )
-        .then((response) => {
-          setSalesRecords(response.data.orders);
-          console.log();
-        })
-        .catch((error) => {
-          // Handle any errors that occur during the fetch
-          console.error("Error fetching order status:", error);
-        });
+        // Fetch data from backend when component mounts
+        axios
+            .get(
+                `http://localhost:8090/api/v1/salesRecord?user_ID=${localStorage.getItem("userId")
+                    ? localStorage.getItem("userId")
+                    : "66189fd589a3a818a791a9c2"
+                }`
+            )
+            .then((response) => {
+                setSalesRecords(response.data.orders);
+                console.log();
+            })
+            .catch((error) => {
+                // Handle any errors that occur during the fetch
+                console.error("Error fetching order status:", error);
+            });
     }, []);
-  
-  
+
+
     useEffect(() => {
-      // Fetch order statuses for all orders in salesRecords
-      const fetchStatuses = async () => {
-        const statuses = await Promise.all(
-          salesRecords.map(record => fetchOrderStatuses(record.order_ID))
-        );
-        // Update orderStatuses state with the fetched statuses
-        setOrderStatuses(statuses);
-      };
-      // Call fetchStatuses when component mounts
-      fetchStatuses();
+        // Fetch order statuses for all orders in salesRecords
+        const fetchStatuses = async () => {
+            const statuses = await Promise.all(
+                salesRecords.map(record => fetchOrderStatuses(record.order_ID))
+            );
+            // Update orderStatuses state with the fetched statuses
+            setOrderStatuses(statuses);
+        };
+        // Call fetchStatuses when component mounts
+        fetchStatuses();
     }, [salesRecords]);
-    
-  
+
+
     const fetchOrderStatuses = async (orderId) => {
-      try {
-        const response = await axios.get(`http://localhost:8090/api/v1/getOrderStatus?orderID=${orderId}`);
-        return response.data.status; // Return status from response
-      } catch (error) {
-        console.error("Error fetching order status:", error);
-        return ""; // Return an empty string in case of error
-      }
-    };
-  
-  
-  
-    const fetchSalesRecords = async () => {
-      try {
-        const response = await fetch("/sales-records");
-        if (!response.ok) {
-          throw new Error("Failed to fetch data");
+        try {
+            const response = await axios.get(`http://localhost:8090/api/v1/getOrderStatus?orderID=${orderId}`);
+            return response.data.status; // Return status from response
+        } catch (error) {
+            console.error("Error fetching order status:", error);
+            return ""; // Return an empty string in case of error
         }
-        const data = await response.json();
-        setSalesRecords(data);
-      } catch (error) {
-        console.error("Error fetching sales records:", error);
-      }
     };
-  
+
+
+
+    const fetchSalesRecords = async () => {
+        try {
+            const response = await fetch("/sales-records");
+            if (!response.ok) {
+                throw new Error("Failed to fetch data");
+            }
+            const data = await response.json();
+            setSalesRecords(data);
+        } catch (error) {
+            console.error("Error fetching sales records:", error);
+        }
+    };
+
     const handleRefundRequest = (orderId) => {
-      navigate("/");
-      console.log("Requesting refund for order:", orderId);
+        navigate("/");
+        console.log("Requesting refund for order:", orderId);
     };
-  
+
     return (
-      <div>
-        <h2>Order History</h2>
-        <table>
-          <thead>
-            <tr>
-              <th>Order ID</th>
-              <th>Date</th>
-              <th>Amount</th>
-              <th>Components</th>
-              <th>Action</th>
-              <th>Refund Status</th>
-            </tr>
-          </thead>
-          <tbody>
-            {salesRecords.map((record, index) => {
-              
-  
-              return (
-                <tr key={record.order_ID}>
-                  <td>{record.order_ID}</td>
-                  <td>{record.order_date}</td>
-                  <td>{record.order_amount}</td>
-                  <td>{record.components.join(", ")}</td>
-                  <td>
-                    <button
-                      style={{
-                        display: "block",
-                        width: "100%",
-                        padding: "10px",
-                        marginTop: "10px",
-                        fontSize: "16px",
-                        color: "#fff",
-                        backgroundColor: "#007bff",
-                        border: "none",
-                        borderRadius: "5px",
-                        cursor: "pointer",
-                      }}
-                      onClick={() => handleRefundRequest(record.order_ID)}
-                    >
-                      Request Refund
-                    </button>
-                  </td>
-                  <td>{orderStatuses[index]}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </div>
+        <div>
+            <h2>Order History</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Order ID</th>
+                        <th>Date</th>
+                        <th>Amount</th>
+                        <th>Components</th>
+                        <th>Action</th>
+                        <th>Refund Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {salesRecords.map((record, index) => {
+
+
+                        return (
+                            <tr key={record.order_ID}>
+                                <td>{record.order_ID}</td>
+                                <td>{record.order_date}</td>
+                                <td>{record.order_amount}</td>
+                                <td>{record.components.join(", ")}</td>
+                                <td>
+                                    <button
+                                        style={{
+                                            display: "block",
+                                            width: "100%",
+                                            padding: "10px",
+                                            marginTop: "10px",
+                                            fontSize: "16px",
+                                            color: "#fff",
+                                            backgroundColor: "#007bff",
+                                            border: "none",
+                                            borderRadius: "5px",
+                                            cursor: "pointer",
+                                        }}
+                                        onClick={() => handleRefundRequest(record.order_ID)}
+                                    >
+                                        Request Refund
+                                    </button>
+                                </td>
+                                <td>{orderStatuses[index]}</td>
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
+        </div>
     );
-  }
-  
+}
+
 
 
 
