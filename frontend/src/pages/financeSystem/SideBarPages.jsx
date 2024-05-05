d2b7c61068e46cc564d769c03733a793bc025887
DGTharaka <166516972+DGTharaka@users.noreply.github.com>
2024-05-05 14:09:53 +0530

    Update SideBarPages.jsx

--- frontend/src/pages/financeSystem/SideBarPages.jsx
+++ frontend/src/pages/financeSystem/SideBarPages.jsx
@@ -10,17 +10,19 @@ export function DashboardView(props) {
             image: avatar,
             altText: "Avatar 1",
             count: "Finance Manager",
-            name: "Michael Brown"
+            name: "Michael Brown",
+            email: "michael.brown@example.com" 
         },
         
     ];
 
     return (
         <>
-            <main>
-                <div className="head-title">
-                    <div className="left">
+            <main style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+                <div>
+                    <div className="head-title" style={{ textAlign: 'center' }}>
                         <Typography variant="h1">Dashboard</Typography>
+                        <Typography variant="body1">{dataList[0].email}</Typography>
                     </div>
                     <div style={{ display: 'flex', justifyContent: 'center' }}> 
                         <CardComp data={dataList} style={{ width: '300px' }} />
