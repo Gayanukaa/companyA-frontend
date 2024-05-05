f887cd4248bada1c2022e04ca433cd112ea75630
DGTharaka <166516972+DGTharaka@users.noreply.github.com>
2024-05-05 13:37:47 +0530

    Dashboard, Finance System

--- frontend/src/pages/financeSystem/SideBarPages.jsx
+++ frontend/src/pages/financeSystem/SideBarPages.jsx
@@ -1,73 +1,26 @@
 import React from "react";
 // import { useEffect } from 'react';
-import CardComp from "../../components/sideComps/CardComp";
-import TableComp from '../../components/sideComps/TableComp'
 import avatar from '../../assets/avatar.svg';
+import './DashBoardView.css';
 
 
 
-export function ViewStocks(props) {
+const DashboardView = () => {
+    return(
+        <div className="UP">
+            <div className="gradient"></div>
+            <div className="profile-down">
+                <img src={avatar} alt="" />
+                <div className="profile-title">Michael Brown</div>
+                <div className="description">Financial Manager</div>
+            </div>
+            <div className="profile-button"><a href="mailto: michael.brown@example.com" className="mail">Contact Me</a></div>
 
-    const tableData = {
-        name: "Sample Table",
-        heading: ["Column 1", "Column 2", "Column 3"],
-        body: [
-            <tr key="row1">
-                <td>Data 1</td>
-                <td>Data 2</td>
-                <td>Data 3</td>
-            </tr>,
-            <tr key="row2">
-                <td>Data 4</td>
-                <td>Data 5</td>
-                <td>Data 6</td>
-            </tr>,
-            // Add more rows as needed
-        ],
-    };
 
-    return (
-        <>
-            <main>
-                <div className="head-title">
-                    <div className="left">
-                        <h1>View Stocks</h1>
-                    </div>
-
-                    <TableComp data={tableData} />
-
-                </div>
-
-            </main>
-        </>
+        </div>
     )
+
 }
 
-
-export function DashboardView(props) {
-
-    const dataList = [
-        {
-            image: avatar,
-            altText: "Avatar 1",
-            count: 5,
-            name: "Michael Brown"
-        },
-        
-    ];
-
-    return (
-        <>
-            <main>
-                <div className="head-title">
-                    <div className="left">
-                        <h1>Dashboard</h1>
-                    </div>
-
-                    <CardComp data={dataList} />
-                </div>
-            </main>
-        </>
-    )
-}
+export default DashboardView    
 
