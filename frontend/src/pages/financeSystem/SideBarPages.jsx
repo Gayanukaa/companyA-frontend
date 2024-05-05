7372e42d3e91155f8f7e01d6d8d74ba64d2e2cad
DGTharaka <166516972+DGTharaka@users.noreply.github.com>
2024-05-05 14:18:41 +0530

    Update SideBarPages.jsx

--- frontend/src/pages/financeSystem/SideBarPages.jsx
+++ frontend/src/pages/financeSystem/SideBarPages.jsx
@@ -1,15 +1,18 @@
 import React from "react";
+// import { useEffect } from 'react';
 import CardComp from "../../components/sideComps/CardComp";
 import TableComp from '../../components/sideComps/TableComp'
 import avatar from '../../assets/avatar.svg';
-import Typography from '@mui/material/Typography'; // Import Typography
+
+
 
 export function DashboardView(props) {
+
     const dataList = [
         {
             image: avatar,
             altText: "Avatar 1",
-            count: "Finance Manager",
+            count: 5,
             name: "Michael Brown"
         },
         
@@ -20,9 +23,10 @@ export function DashboardView(props) {
             <main>
                 <div className="head-title">
                     <div className="left">
-                        <Typography variant="h1">Dashboard</Typography>
+                        <h1>Dashboard</h1>
                     </div>
-                    <CardComp data={dataList}/>
+
+                    <CardComp data={dataList} />
                 </div>
             </main>
         </>
